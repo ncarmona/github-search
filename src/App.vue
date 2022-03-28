0b24398991253b99afb6e740b417357b300ccb5f
Noel Carmona <hello@noelcarmona.com>
2022-03-28 14:55:39 +0200

    Render input search and table results in the app component.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>App</div>
+  <InputSearchRepositoryComponent />
+  <Suspense>
+    <template #default>
+      <RepositoryResultsComponent />
+    </template>
+    <template #fallback>
+      <span>Loading</span>
+    </template>
+  </Suspense>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import InputSearchRepositoryComponent from './components/InputSearchRepositoryComponent.vue'
+import RepositoryResultsComponent from './components/RespositoryResultsComponent.vue'
+
+export default defineComponent({
+  components: { InputSearchRepositoryComponent, RepositoryResultsComponent }
+})
+</script>
