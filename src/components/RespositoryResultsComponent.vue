18c2de9aaf7d5df31d9b65759d8854c5a7066f56
Noel Carmona <hello@noelcarmona.com>
2022-03-28 14:54:33 +0200

    Repository table component implementation.

---
+++ src/components/RespositoryResultsComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <table>
+    <InputSearchRepositoryHeaderComponent />
+    <tbody>
+      <InputSearchRepositoryRowComponent v-for="repository in repositories" :repository="repository" :key="repository.url" />
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+// import { IRepositoryResult } from '@/interfaces/IRepositoryResult'
+import { computed, defineComponent } from 'vue'
+import InputSearchRepositoryHeaderComponent from './TableRepositoryHeaderComponent.vue'
+import InputSearchRepositoryRowComponent from './TableRepositoryRowComponent.vue'
+import { useStore } from 'vuex'
+
+export default defineComponent({
+  components: { InputSearchRepositoryHeaderComponent, InputSearchRepositoryRowComponent },
+  async setup () {
+    const store = useStore()
+    const repositories = computed(() => store.getters.repositories.repositories)
+    return { repositories }
+  }
+})
+</script>
