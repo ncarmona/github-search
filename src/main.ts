4296c6ccfede184729f3cd0377155c333d4e98e2
Noel Carmona <hello@noelcarmona.com>
2022-03-28 16:04:51 +0200

    Added scss file with custom styles

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/style.scss'
 
 createApp(App).use(store).mount('#app')
